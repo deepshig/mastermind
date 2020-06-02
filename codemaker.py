0844c83c0e50f6df42f326e147be3273b10ec8b1
deepshig <deepshi1104@gmail.com>
2020-06-02 14:05:06 +0530

    Add basic CodeMaker class

---
+++ codemaker.py
@@ -0,0 +1,11 @@
+class CodeMaker:
+    """
+    CodeMaker generates the code for each game.
+    It also gives a feedback on each move of the CodeBreaker
+    """
+
+    def __init__(self):
+        __generate_code(self)
+
+    def __generate_code(self):
+        self.code = [1, 2, 3, 4]
