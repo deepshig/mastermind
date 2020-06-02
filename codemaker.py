509f6b17f633fb51c05ea4983dc1f73a6753a194
deepshig <deepshi1104@gmail.com>
2020-06-02 14:22:15 +0530

    Add logic to analyze the move and give feedback in the code maker

--- codemaker.py
+++ codemaker.py
@@ -1,11 +1,28 @@
+import array as arr
+
+
 class CodeMaker:
     """
     CodeMaker generates the code for each game.
-    It also gives a feedback on each move of the CodeBreaker
+    There are no repetitions allowed in the code, and the move.
+    It also gives a feedback on each move of the CodeBreaker, and maintains its own knowledge
     """
 
     def __init__(self):
         __generate_code(self)
 
     def __generate_code(self):
-        self.code = [1, 2, 3, 4]
+        self.code = arr.array('i', [1, 2, 3, 4])
+
+    def analyze_move(self, move):
+        feedback = arr.array('i', [-1, -1, -1, -1])
+
+        for i in range(0, 4):
+            if self.code[i] == move[i]:
+                feedback[i] = 1
+            elif self.code.count(move[i]) == 1:
+                feedback[i] = 0
+            else:
+                feedback[i] = -1
+
+        return feedback
