ddf4b4d560e80f5ca3cb68759845a78f5e39cb9c
deepshig <deepshi1104@gmail.com>
2020-06-03 16:37:58 +0530

    Add logic to analyse the feedback and generate next move of the codebreaker

--- codebreaker.py
+++ codebreaker.py
@@ -1,4 +1,6 @@
 import array as arr
+import random as rand
+import time
 
 
 class CodeBreaker:
@@ -6,3 +8,81 @@ class CodeBreaker:
     def get_first_move(self):
         self.move = arr.array('i', [4, 6, 2, 1])
         return self.move
+
+    def get_next_move(self, feedback):
+        new_move = arr.array('i', [0, 0, 0, 0])
+        old_move = self.move
+
+        new_move = self.__handle_perfectly_correct_elements(new_move, feedback)
+
+        new_move = self.__handle_correct_color_incorrect_position_elements(
+            new_move, feedback)
+
+        new_move = self.__handle_incorrect_elements(new_move, feedback)
+
+        self.move = new_move
+        return self.move
+
+    def __handle_perfectly_correct_elements(self, new_move, feedback):
+        for i in range(0, 4):
+            if feedback[i] == 1:
+                new_move[i] = self.move[i]
+
+        return new_move
+
+    def __handle_correct_color_incorrect_position_elements(self, new_move, feedback):
+        empty_indices = arr.array('i')
+        values_to_handle = arr.array('i')
+
+        for i in range(0, 4):
+            if feedback[i] == 0:
+                values_to_handle.append(self.move[i])
+            if new_move[i] == 0:
+                empty_indices.append(i)
+
+        if len(values_to_handle) != 0 and len(empty_indices) != 0:
+            rand.seed(time.time_ns())
+
+            for val in values_to_handle:
+                random_index = rand.choice(empty_indices)
+                new_move[random_index] = val
+
+        for i in range(0, 4):
+            if feedback[i] == 0:
+                if self.move[i] == new_move[i]:
+                    empty_indices.remove(i)
+                    random_index = rand.choice(empty_indices)
+
+                    new_move[i], new_move[random_index] = new_move[random_index], new_move[i]
+                    break
+
+        return new_move
+
+    def __handle_incorrect_elements(self, new_move, feedback):
+        values_to_substitute = arr.array('i')
+        for i in range(0, 4):
+            if feedback[i] == -1:
+                values_to_substitute.append(self.move[i])
+
+        new_move = substitute_values(new_move, values_to_substitute)
+        return new_move
+
+
+def substitute_values(move, values_to_substitute):
+    for i in range(0, 4):
+        if move[i] == 0:
+            new_val = rand.randint(1, 6)
+
+            while not can_substitute(move, values_to_substitute, new_val):
+                new_val = rand.randint(1, 6)
+
+            move[i] = new_val
+
+    return move
+
+
+def can_substitute(move, values_to_substitute, new_val):
+    if move.count(new_val) == 0 and values_to_substitute.count(new_val) == 0:
+        return True
+    else:
+        return False
--- tests/test_codebreaker.py
+++ tests/test_codebreaker.py
@@ -14,3 +14,34 @@ def codebreaker_player():
 def test_get_first_move(codebreaker_player):
     expected_move = arr.array('i', [4, 6, 2, 1])
     assert codebreaker_player.get_first_move() == expected_move
+
+
+def test_get_next_move(codebreaker_player):
+    # when last move was perfect, i.e.,
+    # when all elements have feedback value 1
+    perfect_move_feedback = arr.array('i', [1, 1, 1, 1])
+    last_move = arr.array('i', [1, 2, 3, 4])
+    codebreaker_player.move = last_move
+    new_move = codebreaker_player.get_next_move(perfect_move_feedback)
+    assert new_move == last_move
+
+    # when some elements in the last move had matching color
+    # but not matching positions, i.e.,
+    # feedback has value 0 for some elements
+    feedback = arr.array('i', [1, 0, 0, 0])
+    last_move = arr.array('i', [1, 4, 2, 3])
+    codebreaker_player.move = last_move
+    new_move = codebreaker_player.get_next_move(feedback)
+    assert new_move != last_move
+    assert new_move[0] == last_move[0]
+
+    # when some elements are totally wrong in the last move
+    # i.e., feedback has value -1 for some elements
+    feedback = arr.array('i', [1, 0, -1, -1])
+    last_move = arr.array('i', [1, 4, 5, 6])
+    codebreaker_player.move = last_move
+    new_move = codebreaker_player.get_next_move(feedback)
+    assert new_move != last_move
+    assert new_move[0] == last_move[0]
+    assert new_move.count(5) == 0
+    assert new_move.count(6) == 0
