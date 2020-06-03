8208a010d34a077b0f5447c4e857be91b6276527
deepshig <deepshi1104@gmail.com>
2020-06-03 14:47:57 +0530

    Add tests for analyzing the move in the codemaker

--- tests/test_codemaker.py
+++ tests/test_codemaker.py
@@ -14,3 +14,20 @@ def codemaker_player():
 def test_code_generation(codemaker_player):
     expected_code = arr.array('i', [1, 2, 3, 4])
     assert codemaker_player.code == expected_code
+
+
+def test_analyze_move(codemaker_player):
+    all_matching_move = arr.array('i', [1, 2, 3, 4])
+    expected_feedback = arr.array('i', [1, 1, 1, 1])
+    assert codemaker_player.analyze_move(
+        all_matching_move) == expected_feedback
+
+    move_with_only_matching_color = arr.array('i', [3, 2, 1, 4])
+    expected_feedback = arr.array('i', [0, 1, 0, 1])
+    assert codemaker_player.analyze_move(
+        move_with_only_matching_color) == expected_feedback
+
+    move_with_wrong_color = arr.array('i', [3, 2, 1, 6])
+    expected_feedback = arr.array('i', [0, 1, 0, -1])
+    assert codemaker_player.analyze_move(
+        move_with_wrong_color) == expected_feedback
