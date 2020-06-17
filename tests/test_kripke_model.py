cecd78a82ce78e6d140e8dcb0e6d16d6c33241b8
deepshig <deepshi1104@gmail.com>
2020-06-17 17:49:05 +0530

    Refine get_relations test

--- tests/test_kripke_model.py
+++ tests/test_kripke_model.py
@@ -1,4 +1,3 @@
-from relations import all_relations
 import github_com.erohkohl.mlsolver
 from github_com.erohkohl.mlsolver.kripke import World
 import pytest
@@ -6,6 +5,7 @@ import sys
 sys.path.append('../')
 
 from kripke_model import get_corresponding_world, generate_worlds, get_relations  # NOQA
+from relations import expected_relations  # NOQA
 
 
 def test_get_corresponding_world():
@@ -23,5 +23,12 @@ def test_get_corresponding_world():
 
 def test_get_relations():
     worlds = generate_worlds()
+    actual_relations = get_relations(worlds)
 
-    assert get_relations(worlds) == all_relations
+    agent1_relations = actual_relations['1']
+    assert len(agent1_relations) == len(worlds)
+    assert agent1_relations == expected_relations['1']
+
+    agent2_relations = actual_relations['2']
+    assert len(agent2_relations) == len(worlds)*len(worlds)
+    assert agent2_relations == expected_relations['2']
