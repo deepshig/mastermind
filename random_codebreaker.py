eb15a1adbf519300b6f654f9abe163998f9fc9a6
deepshig <deepshi1104@gmail.com>
2020-06-18 13:56:15 +0530

    Generate next moves for Random CodeBreaker

--- random_codebreaker.py
+++ random_codebreaker.py
@@ -16,6 +16,18 @@ class RandomCodeBreaker:
 
         return self.move
 
+    def get_next_move(self, feedback):
+        self.move = []
+        available_choices = [1, 2, 3, 4, 5, 6]
+
+        rand.seed(time.time_ns())
+        for _ in range(0, LENGTH_OF_CODE):
+            val = rand.choice(available_choices)
+            self.move.append(val)
+            available_choices.remove(val)
+
+        return self.move
+
 
 r = RandomCodeBreaker()
 print(r.get_first_move())
--- tests/test_random_codebreaker.py
+++ tests/test_random_codebreaker.py
@@ -20,3 +20,20 @@ def test_get_first_move():
     for i in range(0, 4):
         assert available_elements.count(first_move[i]) != 0
         assert first_move.count(first_move[i]) == 1
+
+
+def test_get_next_move():
+    """
+    1. Test if the length of move is correct
+    2. Test if all the elements of the move belong to the given set
+    3. Test if all the elements appear only once within the move
+    """
+    available_elements = [1, 2, 3, 4, 5, 6]
+
+    codebreaker_player = RandomCodeBreaker()
+    first_move = codebreaker_player.get_next_move([])
+
+    assert len(first_move) == 4
+    for i in range(0, 4):
+        assert available_elements.count(first_move[i]) != 0
+        assert first_move.count(first_move[i]) == 1
